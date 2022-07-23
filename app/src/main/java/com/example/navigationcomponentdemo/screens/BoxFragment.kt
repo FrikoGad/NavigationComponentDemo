15cb52ced29b9c13f4b0f0e9b9f339edc6a39f85
FrikoGad <goliaf551@mail.ru>
2022-07-23 12:56:19 +0300

    dev

--- app/src/main/java/com/example/navigationcomponentdemo/screens/BoxFragment.kt
+++ app/src/main/java/com/example/navigationcomponentdemo/screens/BoxFragment.kt
@@ -1,7 +1,52 @@
 package com.example.navigationcomponentdemo.screens
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import com.example.navigationcomponentdemo.R
+import com.example.navigationcomponentdemo.databinding.FragmentBoxBinding
+import kotlin.random.Random
 
 class BoxFragment :Fragment(R.layout.fragment_box) {
+
+    private lateinit var binding: FragmentBoxBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentBoxBinding.bind(view)
+
+        val color = requireArguments().getInt(ARG_COLOR)
+        binding.root.setBackgroundColor(color)
+
+        binding.btnGoBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        binding.btnOpenSecret.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_boxFragment_to_secretFragment,
+                bundleOf(),
+                navOptions {
+                    anim {
+                        enter = R.anim.second_enter
+                        popEnter = R.anim.second_pop_enter
+                    }
+                }
+            )
+        }
+        binding.btnGenerateNumber.setOnClickListener {
+            val number = Random.nextInt(100)
+            parentFragmentManager.setFragmentResult(REQUEST_CODE, bundleOf(EXTRA_RANDOM_NUMBER to number))
+            findNavController().popBackStack()
+        }
+    }
+
+    companion object {
+        const val ARG_COLOR = "color"
+
+        const val REQUEST_CODE = "RANDOM_NUMBER_REQUEST_CODE"
+        const val EXTRA_RANDOM_NUMBER = "EXTRA_RANDOM_NUMBER"
+    }
 }
--- app/src/main/java/com/example/navigationcomponentdemo/screens/SecretFragment.kt
+++ app/src/main/java/com/example/navigationcomponentdemo/screens/SecretFragment.kt
@@ -1,7 +1,25 @@
 package com.example.navigationcomponentdemo.screens
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.navigationcomponentdemo.R
+import com.example.navigationcomponentdemo.databinding.FragmentSecretBinding
 
 class SecretFragment :Fragment(R.layout.fragment_secret) {
+
+    private lateinit var binding: FragmentSecretBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSecretBinding.bind(view)
+
+        binding.btnCloseBox.setOnClickListener {
+            findNavController().popBackStack(R.id.rootFragment, false)
+        }
+        binding.btnGoBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
